5cd4c16ac004649fa08cba3d84e9e693f9272922
David Harillo Sánchez <dhs.seck@gmail.com>
2019-08-23 01:43:34 +0200

    Test sample babel configuration

---
+++ babel-example/output.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var array = ['a', 'b', 'c'];
+array.forEach(function (val) {
+  return console.log(val);
+});
+var template = React.createElement("div", null);
---
+++ babel-example/source.js
@@ -0,0 +1,4 @@
+const array = ['a', 'b', 'c'];
+array.forEach((val) => console.log(val));
+
+const template = (<div />);
